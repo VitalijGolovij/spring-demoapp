7e69d5d9164ba934c7839638a976e5f4be5a6af9
Vitalij <vitalijgl19@gmail.com>
2023-08-09 12:52:43 +0300

    dto for user

---
+++ src/main/java/ru/example/demoapp/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package ru.example.demoapp.dto;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserDTO {
+    @NotEmpty
+    @Size(min = 1, max = 30, message = "Username size should be greater than 1 and less than 30 characters")
+    private String username;
+
+    @Email
+    private String email;
+
+    private String password;
+
+    private String confirmPassword;
+}
