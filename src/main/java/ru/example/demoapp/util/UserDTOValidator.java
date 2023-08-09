1edc4e637df48f3e4d91df77dfa9bd183297cc5e
Vitalij <vitalijgl19@gmail.com>
2023-08-09 16:50:36 +0300

    added user dto validator

---
+++ src/main/java/ru/example/demoapp/util/UserDTOValidator.java
@@ -0,0 +1,40 @@
+package ru.example.demoapp.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.example.demoapp.dto.UserDTO;
+import ru.example.demoapp.model.User;
+import ru.example.demoapp.sevice.UserDetailServiceImpl;
+
+import java.util.Objects;
+
+@Component
+public class UserDTOValidator implements Validator {
+    private final UserDetailServiceImpl userDetailService;
+
+    public UserDTOValidator(UserDetailServiceImpl userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        UserDTO userDTO = (UserDTO) target;
+
+        if (!userDetailService.isUsernameAvailable(userDTO.getUsername())){
+            errors.rejectValue("username","1",
+                    String.format("name '%s' is already taken", userDTO.getUsername())
+            );
+        }
+        if (!Objects.equals(userDTO.getPassword(), userDTO.getConfirmPassword())){
+            errors.rejectValue("passwordConfirm","2",
+                    "incorrect password confirmation"
+            );
+        }
+    }
+}
