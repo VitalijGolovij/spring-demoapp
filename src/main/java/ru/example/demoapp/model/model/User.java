54b621c7d52b1bfa032c9ebbefe309013c8baf1a
Vitalij <vitalijgl19@gmail.com>
2023-08-09 11:45:15 +0300

    added user model

---
+++ src/main/java/ru/example/demoapp/model/model/User.java
@@ -0,0 +1,31 @@
+package ru.example.demoapp.model.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    @Size(min = 1, max = 30, message = "Username size should be greater than 1 and less than 30 characters")
+    private String username;
+
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+}
