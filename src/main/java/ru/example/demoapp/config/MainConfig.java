49c1cdf2a19731623bb347b9a0dad3c2fd00fbfa
Vitalij <vitalijgl19@gmail.com>
2023-08-09 13:00:31 +0300

    added main config

---
+++ src/main/java/ru/example/demoapp/config/MainConfig.java
@@ -0,0 +1,14 @@
+package ru.example.demoapp.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
