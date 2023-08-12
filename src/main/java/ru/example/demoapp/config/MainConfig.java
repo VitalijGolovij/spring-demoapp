763c63abea6f19a964591727667dc56755d60139
Vitalij <vitalijgl19@gmail.com>
2023-08-12 13:22:30 +0300

    added bean clock

--- src/main/java/ru/example/demoapp/config/MainConfig.java
+++ src/main/java/ru/example/demoapp/config/MainConfig.java
@@ -4,11 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
+
 @Configuration
 public class MainConfig {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public Clock clock(){
+        return Clock.systemUTC();
     }
 }
