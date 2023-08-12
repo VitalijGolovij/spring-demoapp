d6fb30a85e76ad783540fa4af2e6a110b82589a1
Vitalij <vitalijgl19@gmail.com>
2023-08-12 13:30:49 +0300

    controllers refactoring

--- src/main/java/ru/example/demoapp/controller/AuthenticateController.java
+++ src/main/java/ru/example/demoapp/controller/AuthenticateController.java
@@ -1,5 +1,6 @@
 package ru.example.demoapp.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,35 +13,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.example.demoapp.util.convertor.DtoConvertor;
+import ru.example.demoapp.convertor.DtoConvertor;
+import ru.example.demoapp.dto.JwtResponse;
 import ru.example.demoapp.dto.LoginUserDto;
 import ru.example.demoapp.dto.RegisterUserDto;
 import ru.example.demoapp.model.User;
+import ru.example.demoapp.sevice.RegisterService;
 import ru.example.demoapp.sevice.RegisterServiceImpl;
 import ru.example.demoapp.util.*;
-import ru.example.demoapp.util.validator.RegisterUserDTOValidator;
+import ru.example.demoapp.validator.RegisterUserDTOValidator;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("")
+@RequiredArgsConstructor
 public class AuthenticateController {
     private final JWTUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
     private final RegisterUserDTOValidator registerUserDTOValidator;
-    private final RegisterServiceImpl registerService;
+    private final RegisterService registerService;
     private final DtoConvertor dtoConvertor;
-
-    @Autowired
-    public AuthenticateController(JWTUtil jwtUtil, AuthenticationManager authenticationManager, RegisterUserDTOValidator registerUserDTOValidator, RegisterServiceImpl registerService, DtoConvertor dtoConvertor) {
-        this.jwtUtil = jwtUtil;
-        this.authenticationManager = authenticationManager;
-        this.registerUserDTOValidator = registerUserDTOValidator;
-        this.registerService = registerService;
-        this.dtoConvertor = dtoConvertor;
-    }
-
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginUserDto loginUserDto,
--- src/main/java/ru/example/demoapp/controller/UserController.java
+++ src/main/java/ru/example/demoapp/controller/UserController.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.example.demoapp.util.convertor.DtoConvertor;
+import ru.example.demoapp.convertor.DtoConvertor;
 import ru.example.demoapp.dto.UserInfoDto;
-import ru.example.demoapp.util.exception.UserNotFoundException;
+import ru.example.demoapp.exception.UserNotFoundException;
 import ru.example.demoapp.model.User;
 import ru.example.demoapp.sevice.UserServiceImpl;
-import ru.example.demoapp.util.UserErrorResponse;
+import ru.example.demoapp.dto.UserErrorResponse;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl userService;
     private final DtoConvertor dtoConvertor;
@@ -34,12 +34,5 @@ public class UserController {
     public UserInfoDto getUser(@PathVariable Long id){
         User user = userService.getUser(id);
         return dtoConvertor.fromUserToUserInfoDto(user);
-    }
-
-    @ExceptionHandler
-    private ResponseEntity<UserErrorResponse> handleException(UserNotFoundException e){
-        UserErrorResponse response = new UserErrorResponse(e.getMessage());
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
